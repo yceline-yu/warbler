2f65a2789551971e5407d1336f71942a02c2d606
Matt Riese <rise.matt@gmail.com>
2021-05-06 16:23:52 -0700

    wrote 3 tests for users

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, LikedMessage
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -38,7 +38,30 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
+        test_u1 = User(
+            email="test_u1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        test_u2 = User(
+            email="test_u2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(test_u1)
+        db.session.add(test_u2)
+        db.session.commit()
+
+        self.test_1 = test_u1
+        self.test_2 = test_u2
+
         self.client = app.test_client()
+
+    def tearDown(self):
+        """ clean test database for next test """
+        db.session.rollback()
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -55,3 +78,44 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_repr(self):
+        """Does the repr method work as expected?"""
+
+        new_user = User(
+            email="repr@test.com",
+            username="testuser_repr",
+            password="HASHED_PASSWORD"
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        self.assertEqual(f"{new_user}", f"<User #{new_user.id}: {new_user.username}, {new_user.email}>")
+
+    def test_is_following(self):
+        """Does is_following() successfully detect when 'user1' is following 'user2'?
+        and does is_following() successfully detect if 'user1' is not following 'not_followed'"""
+
+        follow_rlshp = Follows(
+            user_being_followed_id=self.test_2.id,
+            user_following_id=self.test_1.id)
+
+        db.session.add(follow_rlshp)
+        db.session.commit()
+
+        self.assertEqual(self.test_1.is_following(self.test_2), True)
+        self.assertEqual(self.test_2.is_following(self.test_1), False)
+
+    def test_is_followed_by(self):
+        """Does is_followed_by detect when 'user2' is followed by 'user1'
+        and does it detect that 'user1' is not followed by 'user2'"""
+
+        follow_rlshp = Follows(
+            user_being_followed_id=self.test_2.id,
+            user_following_id=self.test_1.id)
+
+        db.session.add(follow_rlshp)
+        db.session.commit()
+
+        self.assertEqual(self.test_2.is_followed_by(self.test_1), True)
+        self.assertEqual(self.test_1.is_followed_by(self.test_2), False)
