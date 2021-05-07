3e2aa1e322bcd12aeb683d07486e02ca68594b73
yceline-yu <cellystuff5@gmail.com>
2021-05-07 16:42:28 -0700

    url fix

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+database_url = database_url.replace('postgres://', 'postgresql://')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
